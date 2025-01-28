2d01c6dcfc0d96f76a1685561b1b47b07cf818f7
William Boström <133894488+WilliamBostrom@users.noreply.github.com>
2025-01-28 21:24:10 +0100

    day 2 start

---
+++ 2/tasks.py
@@ -0,0 +1,18 @@
+# Subscripting
+print("hello"[-1])
+
+#string
+print("123" + "345")
+
+#integer
+print(123 + 345)
+
+
+#large integer
+print(123_456_789)
+
+#float
+print(3.14159)
+
+#boolean
+print(True)
