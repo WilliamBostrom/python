adec1f850f9f46e7bd554f417d77df0f94f0cd23
William Boström <133894488+WilliamBostrom@users.noreply.github.com>
2025-01-28 21:09:01 +0100

    day 1

---
+++ 1/tasks.py
@@ -0,0 +1,11 @@
+# Task 1
+# print(len(input("Enter your name: ")))
+
+# username = input("Enter your name: ")
+# length = len(username)
+# print(length)
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
