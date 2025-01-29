f9d71109400a0b838f752cff1537427d88fb7511
William Boström <133894488+WilliamBostrom@users.noreply.github.com>
2025-01-29 21:29:16 +0100

    Add tip calculator functionality and clean up code

--- 2/tasks.py
+++ 2/tasks.py
@@ -1,18 +1,69 @@
-# Subscripting
-print("hello"[-1])
+# TIP CALCULATOR
 
-#string
-print("123" + "345")
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+tip = total_bill * (tip_percentage / 100)
+total = total_bill + tip
+each_person = total / people
+each_person = round(each_person, 2)
+print(f"Each person should pay: ${each_person}")
 
-#integer
-print(123 + 345)
 
 
-#large integer
-print(123_456_789)
 
-#float
-print(3.14159)
+# # Subscripting
+# print("hello"[-1])
 
-#boolean
-print(True)
+# #string
+# print("123" + "345")
+
+# #integer
+# print(123 + 345)
+
+
+# #large integer
+# print(123_456_789)
+
+# #float
+# print(3.14159)
+
+# #boolean
+# # print(len("hello") == 1)
+
+# print(type(123) == int)
+
+
+# num = int("123")
+# print(num + int("345"))
+
+# print("Number of letters in your name: " + str(len(input("What is your name? "))))
+
+
+# print("my age: " + str(10))
+# print(123 + 456)
+# print(7 - 3)
+# print(3 * 2)
+# print(122 / 3)
+# print(122 // 3)
+# print(6**2)
+
+# bmi = 100 / (2.00 ** 2)
+# print(bmi)
+
+# print(int(bmi))
+
+# print(round(bmi, 2))
+
+# print(6 + 4 / 2 - (1 * 2))
+
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+# print("Hello, " + name)
+
+# age = 12
+# print(f"Hello! You are {age} years old.")
+
+# print("You are" + age + "years old.")
